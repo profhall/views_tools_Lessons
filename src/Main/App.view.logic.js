197bae865515a8b97f15a86d7cfc6170213935e5
Phillip Hall <phillip_hall@homedepot.com>
2019-03-12 15:30:04 -0400

    have the final Views for the app V1 done, now adding game logic

--- src/Main/App.view.logic.js
+++ src/Main/App.view.logic.js
@@ -1,38 +1,99 @@
 import React from 'react'
 import App from './App.view.js'
+import { matchItems } from '../defaultProps';
+
 
 export default class AppLogic extends React.Component {
   constructor(props){
     super(props);
     console.log("Hi from constructor")
     this.state = {
+      matchValues:null,
       isLoading : true,
-      isReady : false
+      isReady : false,
+      matchCard: null,
+      matchCard_selected: null,
+      matchingCard:null,
+      correctMatches: 0
     };
+    this.increment = this.increment.bind(this)
+    this.shuffle = require('shuffle-array')
+
 
   }
 
-  componentDidMount() {
+  async componentDidMount() {
 
     setTimeout(function () {
-      this.setState({isLoading:false, isReady:true}, ()=>{console.log("Hello after state set")});
-    }.bind(this), 3000);
+      this.setState(
+          {
+            isLoading:false,
+            isReady:true,
+            matchValues:this.shuffle(matchItems)
+          },
+          ()=>{console.log("Hello after state set")});
+    }.bind(this), 1500);
+
+
 
     console.log("Hello from after mounting")
   }
 
+  increment = () => {
+    this.setState(prevState => ({
+      correctMatches: prevState.correctMatches + 1
+    }));
+    console.log("increment ");
+
+  };
+
+  decrement = () => {
+    this.setState(prevState => ({
+      correctMatches: prevState.correctMatches - 1
+    }));
+    console.log("decrement ");
+
+  };
 
   cardClick = card =>{
+    const cardValue = card.target.innerText;
+    const cardID = card.target.id;
     console.log("card Clicked ");
+    console.log(cardValue);
     console.log(card.target);
+
+    if(!this.state.matchCard_selected){
+
+      this.setState(prevState => (
+
+          {
+            matchCard: cardValue,
+            matchCard_ID:cardID,
+            matchCard_selected: !prevState.matchCard_selected
+          }),
+          ()=>
+          {
+            console.log(this.state.matchCard);
+            console.log("is match card selected "+this.state.matchCard_selected + " with ID: "+ this.state.matchCard_ID);
+          });
+    }
+    else{
+      console.log(this.state.matchCard);
+    }
+
+    this.increment()
   };
 
   render() {
-    console.log("Hello from render")
+    console.log("render loaded");
     return <App
         cardClick={this.cardClick}
         isLoading = {this.state.isLoading}
         isReady = {this.state.isReady}
+        score={this.state.correctMatches}
+        matchCardValue={this.state.matchCard ? (this.state.matchCard + " :: " + this.state.matchCard_ID) : 'not selected' }
+        compareCardValue={this.state.matchCard_selected ? "matchingCard" : 'not selected' }
+        matchValues={this.state.matchValues}
         {...this.props}
     />
   }
--- src/defaultProps.js
+++ src/defaultProps.js
@@ -1,6 +1,3 @@
 
-const defaultProps = {
-    from: [1,2,3,4,5,6,7,8,9,10]
-};
+export const matchItems = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9];
 
-export default defaultProps;
